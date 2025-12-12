98b84180978e21af2f41c0ceba33416f67f8fbf1
Phong Tran Nguyen <n.tranphongse@gmail.com>
2025-12-12 18:33:12 +0700

    [MEX-79] Init Repository

---
+++ MyExpenses.Domain/Core/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace MyExpenses.Domain.Core
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.CreateVersion7();
+        public bool IsDeleted { get; set; } = false;
+        public Guid CreatedBy { get; set; }
+        public DateTimeOffset CreatedTime { get; set; }
+        public Guid UpdatedBy { get; set; }
+        public DateTimeOffset UpdatedTime { get; set; }
+    }
+}
---
+++ MyExpenses.Repository/EntityRepository.Utilities.cs
@@ -0,0 +1,17 @@
+﻿using MyExpenses.Domain.Core;
+
+namespace MyExpenses.Repository
+{
+    public partial class EntityRepository<TEntity> where TEntity : BaseEntity
+    {
+        #region Private Utilities
+        protected IQueryable<TEntity> AddDeletedFilter(IQueryable<TEntity> query,
+            in bool includeDeleted = false)
+        {
+            if (includeDeleted) return query;
+
+            return query.Where(e => !e.IsDeleted);
+        }
+        #endregion
+    }
+}
